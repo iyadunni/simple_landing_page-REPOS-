08fb27fa21363f2022bdecf8a15fb101f1dd1f72
Joseph Morenike <josephmorenike2@gamil.com>
2022-07-08 15:04:23 +0100

    A landing page

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn')
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.add('active');
+} );
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
